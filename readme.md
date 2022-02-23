a4c1a4091f796bcf4acbb07f375537dc78cbaa6a
Lawrence <lawrencesalinas@yahoo.com>
2022-02-23 18:04:18 -0500

    created a deck class

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Blackjack Game
+
+* create a simple text-based BlackJack game
+* The game needs to have one player versus an automated dealer.
+* The player can stand or hit.
+* The player must be able to pick their betting amount.
+* need to keep track of the player's total money.
+* need to alert the player of wins, losses, or busts, etc...
+
+And most importantly:
+
+* You must use OOP and classes in some portion of your game. You can not just use functions in your game. Use classes to help you define the Deck and the Player's hand. 
+
+
+# War Game
+
+* Two players will each start off with half the deck, then they each remove a card.
+* compare which card has the highest value, and the player with the higher card wins both cards.
--- war-game.py
+++ war-game.py
@@ -1,9 +1,11 @@
+import random
+
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 
             'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
-# CARD
+# Card class
 class Card():
     def __init__(self, suit, rank):
         self.rank = rank
@@ -12,6 +14,35 @@ class Card():
         
     def __str__(self):
         return f"{self.rank} of {self.suit}"
+    
+    
+# Deck Class
+# instantiate a new deck
+# 52 card objects, shuffle deck, pop method from cards list
+class Deck():
+    def __init__(self):
+        # Note this only happens once upon creation of a new Deck
+        self.all_cards = [] 
+        for suit in suits:
+            for rank in ranks:
+                # This assumes the Card class has already been defined!
+                self.all_cards.append(Card(suit,rank))
+                
+                
+    def shuffle(self):
+         # Note this doesn't return anything
+        random.shuffle(self.all_cards)
+        
+    def deal_one(self):
+        # Note we remove one card from the list of all_cards
+        return self.all_cards.pop()
+
+new_deck = Deck()
+print(new_deck.shuffle)
+# print(new_deck.all_cards)
+                
+            
+        
     
 
 
