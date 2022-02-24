48a9bb3a902b2e7244a11726774e4a3209884b68
Lawrence <lawrencesalinas@yahoo.com>
2022-02-24 02:45:53 -0500

    hand class created

--- blackjack.py
+++ blackjack.py
@@ -29,22 +29,68 @@ class Deck:
                 
     
     def __str__(self):
-        deck_comp = ''
+        deck_comp = ''  # start with an empty string
         for card in self.deck:
-            deck_comp += '\n' + card.__str__()
-        return "the deck has:" + deck_comp
-        
+            deck_comp += '\n '+card.__str__() # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
     def shuffle(self):
         random.shuffle(self.deck)
         
-    def deal_one(self):
+    def deal(self):
         single_card = self.deck.pop()
         return single_card
     
 # test_deck = Deck()
 # test_deck.shuffle()
 # print(test_deck)
+
+
+# A class to hold card objects dealt from the deck
+# will be used to calculate  the value of those card using the values dictionary defined above.
+# adjust values of aces
+class Hand:
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    def add_card(self,card):
+        # card passed in 
+        # from Deck.deal() ---> single Card(suit, rank)
+        self.cards.append(card)
+        self.value += values[card.rank]
         
+        # track aces
+        if card.rank == "Ace":
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        # if total value > 21 and I still have an ace
+        # then change ace to be a 1 instead of an 11
+        while self.value > 21 and self.aces > 0:
+        # change ace to 1, 11 - 10
+            self.value -= 10
+            self.aces -= 1 
+        
+# test_deck = Deck()
+# test_deck.shuffle()
+# Player
+# test_player = Hand()
+# Deak 1 card from the deck
+# pulled_card = test_deck.deal()
+# print(pulled_card)
+# test_player.add_card(pulled_card)
+# print(test_player.value)
+
+# Chips class
+# to keep track of a Player's starting chips, bets, and ongoing winnings
+         
+
+        
+    
+    
+    
      
             
         
